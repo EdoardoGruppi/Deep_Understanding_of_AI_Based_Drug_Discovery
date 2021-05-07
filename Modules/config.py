57246528accd510b75f9dce20e0d339fe8514ec5
Edoardo Gruppi <edoardogruppi@gmail.com>
2021-05-07 09:48:26 +0200

    Add one line try-except function

--- Modules/config.py
+++ Modules/config.py
@@ -1,1 +1,21 @@
 # File containing all the global variables
+# It includes also some useful function non strictly related to the project itself
+
+base_dir = './Datasets'
+
+
+def try_except(function, expected_exc, result=True, default=False):
+    """
+    Try and Except statement in a single line.
+
+    :param function: function to try.
+    :param expected_exc: exception to monitor.
+    :param result: result returned if no error occurs. default_value=True
+    :param default: result returned if an error occurs. default_value=False
+    :return:
+    """
+    try:
+        function()
+        return result
+    except expected_exc:
+        return default
