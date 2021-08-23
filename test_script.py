4346884c8690972c6e05a7e752eddac64810607d
EdoardoGruppi <edoardo.gruppi.20@ucl.ac.uk>
2021-08-23 17:33:46 +0200

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -13,19 +13,21 @@ import sys
 datasets_dir = base_dir
 # Path to the directory where the results are saved
 savings_dir = os.path.join('Savings', 'Saved Results')
-# Filenames of the generated .txt files, one smiles string per line
-reference_dataset_filename = 'CHEMBL_FULL_DATASET.txt'
-test_dataset_filename = 'molRNN_molecules.txt'
-save_filename = 'molRNN_metrics.txt'
+# .txt file that includes the SMILES strings of the training dataset
+reference_dataset_filename = 'chembl_no_actives.txt'
+# Pickle file containing the fingerprints of the reference dataset
+fingerprints_file = 'Datasets/chembl_no_actives_fingerprints.pkl'
+# Filename of the .txt file of the generated molecules, one smiles string per line
+test_dataset_filename = 'new_ck_ppara_molecules_2.txt'
+save_filename = test_dataset_filename.replace('molecules', 'metrics')
+# Number of training samples involved in the computation of the FCD and KL metrics
 subset_size = 2000
 # ------------------------------------------------------------------
 
-# Savings directory path
-directory = os.path.join('Savings', 'Saved Results')
 # Create directory if it does not already exist
-os.makedirs(directory, exist_ok=True)
+os.makedirs(savings_dir, exist_ok=True)
 # Open the txt file to write all the results achieved
-file = open(os.path.join(directory, save_filename), "w")
+file = open(os.path.join(savings_dir, save_filename), "w")
 sys.stdout = file
 
 # Path to the datasets
@@ -46,21 +48,29 @@ print(f'The {ratio2 * 100:5.2f} % of molecules in the test dataset are valid.\n'
 file.flush()
 
 # Novelty and Uniqueness computation
-novelty_value = novelty(train_file=ref_dataset, test_file=test_dataset)
 uniqueness_value = uniqueness(test_file=test_dataset)
+novelty_value = novelty(train_file=ref_dataset, test_file=test_dataset)
 file.flush()
 
-# # Comparison between the property distributions
-# for prop in ['logP', 'MW', 'QED', 'SA', 'NP', 'Atoms']:
-#     property_distributions([
-#         os.path.join(base_dir, 'CHEMBL_FULL_DATASET.txt'),
-#         os.path.join(base_dir, 'HierVAE_molecules.txt'),
-#         os.path.join(base_dir, 'Yasonik_molecules.txt')
-#         ],
-#         list_names=['CHEMBL', 'HierVAE', 'Yasonik'], prop=prop, txt=True)
+# Comparison between the property distributions
+for prop in ['logP', 'MW', 'QED', 'SA', 'NP', 'Atoms']:
+    property_distributions([ref_dataset, test_dataset],
+                           list_names=['Reference', 'Generated'], prop=prop, txt=True)
 
 # Compute the FCD score
 FCD_score, fcd_score = frechet_distance(ref_dataset, test_dataset, sample_size=subset_size)
+file.flush()
+
+# Compute the KL divergence value
+kl_score = kl_divergence(ref_dataset, test_dataset, subset_size=subset_size)
+file.flush()
+
+# Compute SNN and DfD metrics of the generated molecules
+snn, dfd = snn_metric(fingerprints_file, test_dataset, pkl=True, batch_size=500)
+file.flush()
+
+# Compute Internal Diversity of the generated molecules
+int_div = internal_diversity(test_dataset, p=1)
 file.flush()
 
 # Filtering generated molecules
@@ -72,24 +82,14 @@ print('Filtering also with selected QED and SA scores.')
 _ = filter_molecules(test_dataset, check='complete', filters='pains_filters.txt', qed=0.5, sa=5)
 file.flush()
 
-# Compute Internal Diversity of the generated molecules
-int_div = internal_diversity(test_dataset, p=1)
-file.flush()
-
-# Compute SNN and DfD metrics of the generated molecules
-snn, dfd = snn_metric(ref_dataset, test_dataset)
-file.flush()
-
 # Compute the atom occurrences
 atom_occurrences(test_dataset, atoms_list=chembl_atoms_list)
 file.flush()
 
-# Compute the KL divergence value
-kl_score = kl_divergence(ref_dataset, test_dataset, subset_size=subset_size)
-file.flush()
-
 # Get activity
-results = get_activity(test_dataset, confidence=90, targets=['CHEMBL262'], activity=['active'])
+print('Activity computed by means of the ChEMBL27 model')
+# BACE1 'CHEMBL4822' - PPARalpha 'CHEMBL239' - CDK2 'CHEMBL301' - DRD3 'CHEMBL234'
+results = get_activity(test_dataset, confidence=80, targets=['CHEMBL239'], activity=['active'])
 file.flush()
 
 # Close the file
